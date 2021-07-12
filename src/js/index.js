eb739706227b8c8efce9d0c3ad8ae3866a822764
kostuk42 <kostuk42@i.ua>
2021-07-13 00:13:08 +0300

    finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,14 @@
 import ApiService from "./apiService";
-import { debounce } from "lodash";
 import toastr from "toastr";
 import 'toastr/build/toastr.css';
 import '../css/style.css';
 import cardTemplate from '../templates/cards.hbs';
 import * as basicLightbox from 'basiclightbox';
 import 'basicLightbox/dist/basicLightbox.min.css';
-let apiService = new ApiService(toastr);
-let searchQuery = '';
 import refs from './refs.js';
 
+let apiService = new ApiService(toastr);
+let searchQuery = '';
 refs.inputEl.addEventListener("change", (e) => {
     searchQuery = refs.inputEl.value;
 })
