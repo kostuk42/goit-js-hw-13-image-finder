8df7e5a763b17154df5336c039aed7f7f71ee296
kostuk42 <kostuk42@i.ua>
2021-07-12 15:08:03 +0300

    done

---
+++ src/js/apiService.js
@@ -0,0 +1,82 @@
+export default function ApiService(toastr) {
+    this.key = '22433952-2d63403013f80436a9dd1929b';
+    this.lastQuery = '';
+    this.pageNumber = 1;
+    this.result = '';
+    this.target = '';
+    this.observer = '';
+    this.control = 0;
+    this.toastr = toastr;
+    this.api = async (searchQuery, cardTemplate) => {
+        const galleryEl = document.body.querySelector('.gallery');
+        if (!searchQuery) {
+            // console.log('No choice!');
+            this.toastr.error('No choice!');
+            return
+        }
+        if (this.lastQuery !== searchQuery) {
+            this.pageNumber = 1;
+            this.result = '';
+            galleryEl.innerHTML = null;
+        }
+        if (this.result && this.result.total === this.control) {
+            // console.log('there is nothing to show more');
+            this.toastr.info('there is nothing to show more');
+            return
+        }
+        let URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${this.pageNumber}&per_page=12&key=${this.key}`;
+        let response = await fetch(URL);
+        if (response.status === 404) {
+            console.log('not found');
+            return
+        }
+        let result = await response.json().catch(err => {
+            this.toastr.warning('something went wrong');
+            return
+        })
+        this.lastQuery = searchQuery;
+        if (result.total === 0) {
+            this.toastr.error('Unfortunately we have find nothing :(');
+            return
+        } else if (this.pageNumber === 1) {
+            toastr.info(`we have find ${result.total} matches`);
+        }
+       
+        this.result = result; 
+        result.hits.forEach(pic => {
+             galleryEl.insertAdjacentHTML('beforeend', cardTemplate(pic));
+             const elem = galleryEl.lastChild.querySelector('img');
+             elem.onload = () => {
+                 this.control += 1;
+                 if (this.control === (this.pageNumber - 1) * 12) {
+                     console.log('works');
+                     const scrollElem = galleryEl.children[this.control - 12];
+                     console.log(scrollElem);
+                     scrollElem.scrollIntoView({
+                       behavior: 'smooth',
+                       block: 'start'
+                       });
+                }
+            }
+        });
+        
+            if (this.observer) {
+                this.observer.disconnect(this.target)
+            }
+            this.target = galleryEl.lastElementChild;
+            const options = {
+                threshold: 1
+            }
+            const callback = (entries, observer) => {
+                if (entries[0].isIntersecting) {
+                    if(this.control <12 ) return
+                    this.api(searchQuery, cardTemplate);
+                }
+            }
+            this.observer = new IntersectionObserver(callback, options);
+            if (this.target) {
+                this.observer.observe(this.target);
+           };
+        this.pageNumber += 1;
+    }  
+}
---
+++ src/js/index.js
@@ -0,0 +1,40 @@
+import ApiService from "./apiService";
+import { debounce } from "lodash";
+import toastr from "toastr";
+import 'toastr/build/toastr.css';
+import '../css/style.css';
+import cardTemplate from '../templates/cards.hbs';
+import * as basicLightbox from 'basiclightbox';
+import 'basicLightbox/dist/basicLightbox.min.css';
+let apiService = new ApiService(toastr);
+let searchQuery = '';
+const refs = {
+    inputEl: document.body.querySelector('.search-form input'),
+    buttonEl: document.body.querySelector('.search'),
+    galleryEl: document.body.querySelector('.gallery')
+    
+}
+
+refs.inputEl.addEventListener("change", (e) => {
+    searchQuery = refs.inputEl.value;
+})
+
+refs.buttonEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    apiService.api(searchQuery, cardTemplate);
+});
+console.log(refs.galleryEl)
+refs.galleryEl.onclick = (e) => {
+    console.log(e.target.tagName)
+    if (e.target.tagName !== 'IMG') return
+    const imgEl = e.target;
+    const url = imgEl.dataset.large;
+    const instance = basicLightbox.create(`<img src="${url}" alt="">`);
+    instance.show();
+}
+
+
+
+
+
+
